5ea764fcbf462a7c2fde9aa13e377bfb779db576
1004 <1213453586@qq.com>
2020-07-18 13:05:16 +0800

    2020/7/18/13:05

---
+++ K103_distance/USER/Ks103.h
@@ -0,0 +1,16 @@
+/*
+ * Ks103.h
+ *
+ *  Created on: 2020Äê7ÔÂ18ÈÕ
+ *      Author: 1004
+ */
+
+#ifndef USER_KS103_H_
+#define USER_KS103_H_
+#include "common.h"
+
+void Ks103_init(void);
+int Ks103_getdistance(void);
+
+
+#endif /* USER_KS103_H_ */
